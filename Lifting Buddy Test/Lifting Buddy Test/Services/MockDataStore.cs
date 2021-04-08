c265eb7397a701894291f1374de8935aeb6f9872
Kane Hamilton <kanehamilton00@gmail.com>
2021-04-07 20:20:24 -0400

    Added Workout Names - 5 Arm 5 Shoulder

--- Lifting Buddy Test/Lifting Buddy Test/Services/MockDataStore.cs
+++ Lifting Buddy Test/Lifting Buddy Test/Services/MockDataStore.cs
@@ -14,12 +14,17 @@ namespace Lifting_Buddy_Test.Services
         {
             items = new List<Item>()
             {
-                new Item { Id = Guid.NewGuid().ToString(), Text = "First item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Second item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Third item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Fourth item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Fifth item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Sixth item", Description="This is an item description." }
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Incline Dumbbell Curl", Description="Workout Type: Arm Exercise" },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Standing Barbell Curl", Description="Workout Type: Arm Exercise" },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Seated Concentration Curl", Description="Workout Type: Arm Exercise" },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Skull Crushers", Description="Workout Type: Arm Exercise" },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Hammer Curl", Description="Workout Type: Arm Exercise" },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Dumbbell Lateral Raise", Description="Workout Type: Shoulder Exercise" },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Dumbbell Front Raise", Description="Workout Type: Shoulder Exercise" },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Dumbbell Shrug", Description="Workout Type: Shoulder Exercise" },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Dumbbell Rear Delt Raise", Description="Workout Type: Shoulder Exercise" },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Barbell Military Press", Description="Workout Type: Shoulder Exercise" },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "back", Description="Workout Type: Back Exercise" }
             };
         }
 
